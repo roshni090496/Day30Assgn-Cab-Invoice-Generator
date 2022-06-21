85798df53f5d9f6a1455d2e506c2c93cbac84546
roshni090496 <rs09041001@gmail.com>
2022-06-21 04:53:06 -0700

    Modified : Cab Invoice Generator Code

--- src/test/java/InvoiceServiceTest.java
+++ src/test/java/InvoiceServiceTest.java
@@ -1,4 +1,4 @@
-import org.junit.jupiter.api.Assertions;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
 public class InvoiceServiceTest {
@@ -13,14 +13,14 @@ public class InvoiceServiceTest {
         double distance = 2.0;
         int time = 5;
         double fare = invoiceGenerator.calculateFare((double) distance, (int) time);
-        Assertions.assertEquals(25.0, fare, 1);
+        Assert.assertEquals(25.0, fare, 1);
     }
     @Test
     public void givenLessDistanceTimeReturnMinFare() {
         double distance = 0.1;
         int time = 1;
         double fare = invoiceGenerator.calculateFare((double) time, (int) distance);
-        Assertions.assertEquals(10.0, fare, 1);
+        Assert.assertEquals(10.0, fare, 1);
     }
 
     @Test
@@ -28,7 +28,7 @@ public class InvoiceServiceTest {
         Ride[] rides = new Ride[]{new Ride(2.0, 5), new Ride(0.1, 1)};
         InvoiceSummary summary = invoiceGenerator.calculateFare(rides);
         InvoiceSummary expectedSummary = new InvoiceSummary(2, 30.0);
-        Assertions.assertEquals(expectedSummary, summary);
+        Assert.assertEquals(expectedSummary, summary);
     }
 
 }
